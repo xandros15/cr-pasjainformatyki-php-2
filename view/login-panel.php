0120b46bcbdc8d89ff707f4310db1599f1e2d191
xandros15 <xandros92@gmail.com>
2016-08-09 18:26:46 +0200

    Przeniesienie widoku logowania do innego pliku

---
+++ view/login-panel.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-08-09
+ * Time: 18:24
+ */
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Osadnicy - gra przeglądarkowa</title>
+</head>
+
+<body>
+
+Tylko martwi ujrzeli koniec wojny - Platon<br/><br/>
+
+<form method="post">
+
+    Login: <br/> <input type="text" name="login"/> <br/>
+    Hasło: <br/> <input type="password" name="haslo"/> <br/><br/>
+    <input type="submit" value="Zaloguj się"/>
+
+</form>
+
+<?= isset($_SESSION['blad']) ? $_SESSION['blad'] : ''; ?>
+
+</body>
+</html>
